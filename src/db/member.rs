af275204ea417d00cf789c98f3d4139d3a7932ba
Swayam Agrahari <swayamagrahari025@gmail.com>
2024-11-16 00:27:16 +0530

    added cp leaderbaord struct for member

--- src/db/member.rs
+++ src/db/member.rs
@@ -1,6 +1,5 @@
-use sqlx::FromRow;
 use async_graphql::SimpleObject;
-
+use sqlx::FromRow;
 
 //Struct for the Member table
 #[derive(FromRow, SimpleObject)]
@@ -14,4 +13,7 @@ pub struct Member {
     pub sex: String,
     pub year: i32,
     pub macaddress: String,
+    pub leaderboard_id: String,
+    pub cp_platform: String,
+    pub rating:String,
 }
